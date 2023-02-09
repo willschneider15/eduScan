dfd6e59be3b3ad0797a9dec634277e6b44a72105
vaderlock <smuntase@nyit.edu>
2023-02-09 01:39:42 -0500

    staic page v0.1

--- web-app/pages/index.tsx
+++ web-app/pages/index.tsx
@@ -1,83 +1,135 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import { Link as ScrollLink} from 'react-scroll'
 
 const Home: NextPage = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title> eduScan </title>
+        <link rel="icon" href="/black_no_bg.png" />
       </Head>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
+      <nav className="flex h-24 w-full items-center border-b justify-between">
+        <a
+          className="flex px-60 gap-2"
+          href="https://twitter.com/eduscan_xyz"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image className="hover:scale-125" src="/eduscan_no_bg.png" alt="Vercel Logo" width={250} height={250} />
+        </a>
+
+        <div className="flex px-60 gap-2">
+          <ScrollLink 
+            href="/"
+            to="events" smooth={true} 
+            duration={500}
+            className="text-xl hover:text-purple-600 transition-in duration-500 ease-out 
+            border-b-2 border-transparent hover:border-purple-600 px-3">  
+
+            Events 
+
+          </ScrollLink>
+
+          <button className="text-xl hover:text-purple-700 transition-in duration-500 ease-out 
+            border-b-2 border-transparent hover:border-purple-700 px-3"> Messages </button>
+          <button className="text-xl hover:text-purple-700 transition-in duration-500 ease-out 
+            border-b-2 border-transparent hover:border-purple-700 px-3"> Profile </button>
+          <button className="text-xl hover:text-purple-700 transition-in duration-500 ease-out 
+            border-b-2 border-transparent hover:border-purple-700 px-3"> Settings </button>
+        </div>
+
+    </nav>
+
+      <main className="flex w-full flex-1 items-center justify-center px-20 text-center my-60">
+        
+        <div className="flex w-full flex-row items-center justify-center">
+        
+        <h1 className="flex flex-col text-5xl text-purple-700 font-bold">
+        Stay Connected with the Crypto Community on Campus and Beyond
+        
+        <h2 className="text-black mt-3 text-2xl">
+        powered by <img className="inline-block" src="/sol.png" alt="Solana Logo" width={150} height={150} />
+        </h2>
+
         </h1>
+        
+        </div>
+        
+      </main>
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
+      <div id="events" className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center my-60">
+      
+      <div>
+      <h2 className="text-purple-700 mt-3 text-4xl">
+        Events
+        </h2>
+      </div>
+      
+      <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
           <a
             href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
+            className="mt-6 w-96 rounded-xl border p-6 my-20 text-center hover:text-purple-700 focus:text-blue-600 shadow-xl"
           >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
+            <h3 className="text-2xl font-bold">Event 1</h3>
             <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
+              What is Blockchain?
             </p>
           </a>
 
           <a
             href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
+            className="mt-6 w-96 rounded-xl border p-6 my-20 text-center hover:text-purple-700 focus:text-blue-600 shadow-xl"
           >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
+            <h3 className="text-2xl font-bold">Event 2</h3>
             <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
+              Tools and Technologies in Web3 Development
             </p>
           </a>
 
           <a
             href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
+            className="mt-6 w-96 rounded-xl border p-6 my-20 text-center hover:text-purple-700 focus:text-blue-600 shadow-xl"
           >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
+            <h3 className="text-2xl font-bold">Event 3 </h3>
             <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
+              Are NFT's worth getting into? Find out tomorrow at 3:30 pm
             </p>
           </a>
 
           <a
             href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
+            className="mt-6 w-96 rounded-xl border p-6 my-20 text-center hover:text-purple-700 focus:text-blue-600 shadow-xl"
           >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
+            <h3 className="text-2xl font-bold">Event 4</h3>
             <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              Interested in a Solana Hackathon? Come to the interest session on Friday! 
             </p>
           </a>
         </div>
-      </main>
+      </div>
 
       <footer className="flex h-24 w-full items-center justify-center border-t">
         <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          className="flex items-center justify-center gap-2 px-5"
+          href="https://twitter.com/eduscan_xyz"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+          <Image className="hover:scale-125" src="/twitter.png" alt="Twitter Logo" width={40} height={40} />
         </a>
+
+        <a
+          className="flex items-center justify-center gap-2 px-5"
+          href="https://discord.gg/U4gbtPU5"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image className="hover:scale-125" src="/discord.png" alt="Disccord Logo" width={40} height={40} />
+        </a>
+
       </footer>
     </div>
   )
