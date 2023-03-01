5aa622143bf8f00fe2dcfe753cf5bea37a80153a
willschneider15 <willschneider15@gmail.com>
2023-03-01 01:03:15 -0500

    converting to typescript

--- web-app/lib/sanity.server.ts
+++ web-app/lib/sanity.server.ts
@@ -1,11 +1,18 @@
 import { createClient } from 'next-sanity'
 import { config } from './config'
 
+// Just testing stuff to try to make vercel work with pulling sanity data
+// import { apiVersion, dataset, projectId} from './sanity.api'
+
+// const client = projectId
+//     ? createClient({ projectId, dataset, apiVersion, useCdn: false })
+//   : null
+
 export const sanityClient = createClient({
     dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
     projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
     apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION,
-    useCdn: true,
+    useCdn: false,
     token: process.env.NEXT_PUBLIC_SANITY
 }
 )
@@ -14,7 +21,7 @@ export const previewClient = createClient({
     dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
     projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
     apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION,
-    useCdn: true,
+    useCdn: false,
     token: process.env.NEXT_PUBLIC_SANITY
 })
 
